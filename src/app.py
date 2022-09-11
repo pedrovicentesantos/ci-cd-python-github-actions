6b8e8a7fe0c4dc227065a8fe1a2c93722eb9e15d
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-09-11 15:08:03 -0300

    feat: add PORT env var

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+import os
 import requests
 from flask import Flask, render_template
 
+from dotenv import load_dotenv
+
 app = Flask(__name__)
 
+load_dotenv()
 
 @app.route('/')
 def cat():
@@ -18,3 +22,8 @@ def dog():
     resp_json = resp.json()
     url = resp_json[0]['url']
     return render_template('index.html', title='Dog', image_url=url)
+
+if (__name__ == '__main__'):
+    port = os.getenv('PORT') if os.getenv('PORT') != None else 5000 
+
+    app.run(port=port)
